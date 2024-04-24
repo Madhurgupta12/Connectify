9848efe0163bc7a598f6e5f7dfd09839455d9860
Madhur Gupta <125036882+Madhurgupta12@users.noreply.github.com>
2024-04-24 18:17:55 +0530

    Update requireLogin.js

--- Backend/middleware/requireLogin.js
+++ Backend/middleware/requireLogin.js
@@ -19,7 +19,7 @@ module.exports=(req,res,next)=>{
         req.user=userdata;
         next();
     })
-   
+
 
     
    })
@@ -27,3 +27,30 @@ module.exports=(req,res,next)=>{
 
 
 }
+
+
+// module.exports=(req,res,next)=>{
+//     const {authorization}=req.headers;
+//     //authorization =Bearer efunridvlignr
+//     if(!authorization){
+//        return res.status(401).send({error:"you must be logged in"});
+//     }
+//    const token= authorization.replace("Bearer ","");
+//    jwt.verify(token,JWT_SECRET,(err,payload)=>{
+//     if(err)
+//     {
+//         return res.status(401).json({error:"you must be logged in"});
+//     }
+//     const {_id}=payload
+//     User.findById(_id).then(userdata=>{
+//         req.user=userdata;
+//         next();
+//     })
+
+
+    
+//    })
+
+
+
+// }
